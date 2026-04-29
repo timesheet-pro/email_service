3cad737892b97a10a4ffecf9935511997117fa34
timesheet-pro <timesheetpro@qaoncloud.com>
2026-04-29 15:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
 GMAIL_USER = os.environ.get("GMAIL_USER")
 GMAIL_APP_PASSWORD = os.environ.get("GMAIL_APP_PASSWORD")
